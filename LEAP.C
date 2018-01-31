d72c8882e1ee10f8ddcc6ba57763c47f480c1ad2
poorniii <35651828+poorniii@users.noreply.github.com>
2018-01-31 16:45:03 +0530

    LEAP.C
    CHECK WHETHER A YEAR IS LEAP OR NOT

---
+++ LEAP.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#inclue<conio.h>
+vod main()
+{
+int year;
+printf("enter the year");
+scanf("%d",&year);
+if(%2==0)
+{
+if(%50==0)
+{
+if(%100==0)
+printf("%d is a leap year",year);
+else
+}
+printf(%d is a leap year",year);
+else
+}
+printf(%d is a leap year",year);
+return 0;
+}
